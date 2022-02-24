130aef02f199cbfc66da4cfffd2389f613fa4adc
Gigi-Didberidze <gigididberidze01@gmail.com>
2022-02-24 06:35:03 +0100

    Initial import

---
+++ build/CMakeFiles/testprofiling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testprofiling.dir/main.c.o"
+  "testprofiling"
+  "testprofiling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testprofiling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/testprofiling.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testprofiling.
+# This may be replaced when dependencies are built.
